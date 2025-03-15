e22fd94e8d0d2852a4ccaeb96b3b6f65189dd898
KZDNguyenThanh <nguyentuanthanh.kn@gmail.com>
2025-03-15 15:31:27 +0700

    Them API doi mat khau

--- APi/Login_Api.py
+++ APi/Login_Api.py
@@ -3,6 +3,7 @@ class Login_Api(main_api.Api):
     def __init__(self):
         super().__init__()
         self.connector()
+    
     def check_user_login(self,username,password):
         if not username or not password:
             return -1 # Empty username or password
@@ -11,4 +12,14 @@ class Login_Api(main_api.Api):
             return -2  # User not found
         if user['Password'] != password:
             return -3 # Incorrect password
-        return 0 # Login Sucess
+        return 0 # Login Success
+    
+    def check_admin_email(self,email):
+        check = self.admin_collection.find_one({'Email':email})
+        if not check:
+            return -1 # Email not found
+        else:
+            return 0 # Email found
+        
+    def change_password(self,username,password):
+        self.admin_collection.update_one({'Email':username},{'$set':{'Password':password}})
