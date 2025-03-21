774c6ea6d9efc3364f5ec5ececb6ffd28e55f1e9
Tritran-119 <tktri115599@gmail.com>
2025-03-21 19:44:49 +0700

    Update Add_Book_Create.py

--- Modules/Book/Add/Add_Book_Create.py
+++ Modules/Book/Add/Add_Book_Create.py
@@ -18,8 +18,8 @@ class Add_Book_Create:
         self.window_width = 1280
         self.window_height = 832
         self.window.geometry("%dx%d+%d+%d" % (self.window_width, self.window_height,
-                                              (self.screen_width - self.window_width) / 2,
-                                              (self.screen_height - self.window_height) / 2))
+                                      (self.screen_width - self.window_width) // 2,
+                                      self.window.winfo_y()))
         self.window.configure(bg="#FFFFFF")
         self.window.title('Borrow Book')  
         self.canvas = Canvas(self.window, bg="#ffffff", height=832, width=1280,
